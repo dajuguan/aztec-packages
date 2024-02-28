a60b82b27b89387879f8e5777d70a6f4ef4f3cf3
José Pedro Sousa <outgoing@zpedro.dev>
2024-02-28 12:38:29 +0000

    adding react frontend tests

--- boxes/vanilla-js/playwright.config.ts
+++ boxes/vanilla-js/playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  testMatch: '**.spec.ts',
   fullyParallel: true,
   retries: 3,
   workers: process.env.CI ? 1 : 3,
